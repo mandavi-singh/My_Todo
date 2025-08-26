3dca14ed2f83e823c6fba792a95b991c6fe3517e
mandavi-singh <singhmandavi002@gmail.com>
2025-08-27 01:22:13 +0530

    fix: updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join('/tmp', 'todo.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
