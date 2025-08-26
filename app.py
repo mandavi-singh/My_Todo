5a1d1ad32d08fb823d04e29e45bd5c89acced208
mandavi-singh <singhmandavi002@gmail.com>
2025-08-27 01:51:55 +0530

    Update Flask app with PostgreSQL integration and todo functionality

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'tmp', 'todo.db')
+DATABASE_URL = "postgresql://my_todo_db_cjd8_user:W5Uc7QjE5ReAY9tV2yDlH3p5i7D9xklq@dpg-d2n195vfte5s738t6q80-a.oregon-postgres.render.com/my_todo_db_cjd8"
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL", DATABASE_URL)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
